858f41bd5b7a00fb517596136f670bcfc8a31fe2
aliakborswe <aliakbor.js@gmail.com>
2025-05-15 13:25:25 +0600

    portfolio page added portfolio

---
+++ src/app/(main)/portfolio/page.tsx
@@ -0,0 +1,12 @@
+import Portfolio from '@/components/frontend/portfolio/Portfolio'
+import React from 'react'
+
+
+
+export default function page() {
+  return (
+    <div className='pt-[78px]'>
+      <Portfolio />
+    </div>
+  );
+}
